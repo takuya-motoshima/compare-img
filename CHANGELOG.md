72c3a373d5dcbde02c2aa9f34fee419317747076
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2021-12-02 11:04:35 +0900

    Added experimental code to load crossorigin images

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+<!-- ## [1.0.1] - 2021-12-02
+### Fixed
+- Fixed so that CORS images can also be read and compared. -->
+
 ## [1.0.0] - 2021-12-01
 ### Fixed
 - First release.
+
+<!-- [1.0.1]: https://github.com/takuya-motoshima/compare-img/compare/v1.0.0...v1.0.1 -->
--- src/utils/fetchBlob.ts
+++ src/utils/fetchBlob.ts
@@ -6,8 +6,37 @@
  */
 export default async function(url: string): Promise<Blob> {
   // console.log(`Load ${url}`);
+  // return new Promise<Blob>((rslv, rej) => {
+  //   const img = new Image;
+  //   img.addEventListener('load', () => {
+  //     // Update canvas size to match image
+  //     const cvs = document.createElement('canvas');
+  //     cvs.width = img.naturalWidth;
+  //     cvs.height = img.naturalHeight;
+    
+  //     // Draw in image.
+  //     cvs.getContext('2d')!.drawImage(img, 0, 0);       
+      
+  //     // Get content as blob.
+  //     cvs.toBlob(blob => {
+  //       rslv(blob!);
+  //     }, 'image/png');
+  //   });
+  //   img.addEventListener('error', () => {
+  //     rej(new Error(`Error loading ${url}`));
+  //   });
+
+  //   // If from different origin.
+  //   img.crossOrigin = '';
+  //   img.src = url;
+  // });
+
   // Fetch the specified URL.
-  const res = await fetch(url, {mode: 'cors', cache: 'no-cache'});
+  const res = await fetch(url, {
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'include'
+  });
 
   // Throw an exception if the response is an error. 
   if (!res.ok)
